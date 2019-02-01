963dec8fb6cfc39168e695d8260dd766060906c6
Simon Uhrmann <simon.uhrmann@stud.th-deg.de>
2019-02-01 02:19:55 +0100

    Replaced default layout with "Baseline", pre-made by Vuetify (https://vuetifyjs.com/en/framework/pre-made-layouts)

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,42 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+  <v-app id="inspire">
+    <v-navigation-drawer
+            v-model="drawer"
+            fixed
+            app
+    >
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Contact</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="indigo" dark fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
     </v-toolbar>
-
     <v-content>
+
+      <!-- Page content -->
       <HelloWorld/>
+
     </v-content>
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2017</span>
+    </v-footer>
   </v-app>
 </template>
 
@@ -29,10 +48,12 @@ export default {
   components: {
     HelloWorld
   },
-  data () {
-    return {
-      //
-    }
+  data: () => ({
+    drawer: null
+  }),
+
+  props: {
+    source: String
   }
 }
 </script>
