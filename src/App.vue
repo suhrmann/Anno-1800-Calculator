1c627208a9d8439b48e03c656b569f62669d3df4
Simon Uhrmann <suhrmann@posteo.de>
2019-02-04 23:04:50 +0100

    Installed `vuex` and `vue-router` via vue-cli

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,9 @@
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer
-            v-model="drawer"
-            fixed
-            app
-    >
-      <v-list dense>
-        <v-list-tile @click="">
-          <v-list-tile-action>
-            <v-icon>home</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Home</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile @click="">
-          <v-list-tile-action>
-            <v-icon>contact_mail</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Contact</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar color="rgb(198, 141, 92)" dark fixed app>
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
-    </v-toolbar>
-    <v-content>
-
-      <!-- Page content -->
-      <HelloWorld/>
-
-    </v-content>
-    <Footer></Footer>
-  </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld';
-import Footer from './components/Footer';
-
-export default {
-  name: 'App',
-  components: {
-    Footer,
-    HelloWorld,
-  },
-  data: () => ({
-    drawer: null,
-  }),
-
-  props: {
-    source: String,
-  },
-};
-</script>
