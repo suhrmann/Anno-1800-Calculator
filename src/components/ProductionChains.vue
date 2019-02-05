62d073a7e717ecb82f39e12e8604e6cfe1d30695
tobias.luderer@gmail.com <tobias.luderer@gmail.com>
2019-02-05 21:23:14 +0100

    aligned bottom nav bar to bottom

--- src/components/ProductionChains.vue
+++ src/components/ProductionChains.vue
@@ -1,22 +1,26 @@
 <template>
-  <v-card>
-    <h1>Production Chains</h1>
-
-    <BottomNavBar></BottomNavBar>
-  </v-card>
+  <v-container fluid fill-height>
+    <v-layout row wrap align-content-space-between>
+      <v-flex xs12>
+        <h1>Production Chains</h1>
+      </v-flex>
+      <v-flex xs12>
+        <BottomNavBar></BottomNavBar>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-import BottomNavBar from './BottomNavBar';
+import BottomNavBar from "./BottomNavBar";
 
 export default {
-  name: 'ProductionChains',
+  name: "ProductionChains",
   components: {
-    BottomNavBar,
-  },
+    BottomNavBar
+  }
 };
 </script>
 
 <style scoped>
-
 </style>
