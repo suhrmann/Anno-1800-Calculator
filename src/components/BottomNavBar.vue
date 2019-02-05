2eb3a17e19c5fc741ff39d2de2e789008570a178
Simon Uhrmann <suhrmann@posteo.de>
2019-02-05 23:42:24 +0100

    Implemented production chain selection using multiple `v-bottom-nav`

--- src/components/BottomNavBar.vue
+++ src/components/BottomNavBar.vue
@@ -2,23 +2,47 @@
 
   <v-flex xs12>
 
-    <v-bottom-nav
-      :active.sync="selectedWorld"
-      :value="true"
-      absolute
-      dark
-    >
-      <v-btn
-        color="primary"
-        flat
-        v-for="(world, i) in worlds"
-        :value="world"
+    <!-- Nav Bar: TIER -->
+    <v-card height="56px">
+      <v-bottom-nav
+        :active.sync="selectedSocialClassName"
+        :value="true"
+        absolute
+        dark
       >
-        <span>{{ world.name }}</span>
-        <v-icon>history</v-icon>
-      </v-btn>
+        <v-btn
+          color="primary"
+          flat
+          v-for="(tier, i) in selectedWorld.socialClasses"
+          :value="tier.name"
+        >
+          <span>{{ tier.name }}</span>
+          <img :src="tier.img" :alt="tier.name + ' Image'">
+        </v-btn>
 
-    </v-bottom-nav>
+      </v-bottom-nav>
+    </v-card>
+
+    <!-- Nav Bar: WORLD -->
+    <v-card height="55px">
+      <v-bottom-nav
+        :active.sync="selectedWorldName"
+        :value="true"
+        absolute
+        dark
+      >
+        <v-btn
+          color="primary"
+          flat
+          v-for="(world, i) in worlds"
+          :value="world.name"
+        >
+          <span>{{ world.name }}</span>
+          <img :src="world.img" :alt="world.name + ' Image'">
+        </v-btn>
+
+      </v-bottom-nav>
+    </v-card>
 
   </v-flex>
 </template>
@@ -30,15 +54,22 @@ export default {
   name: 'BottomNavBar',
   data() {
     return {
+
       // Init selection
-      selectedWorld: null,
-      selectedSocialClass: null,
+      selectedWorldName: 'Old World',
+      selectedSocialClassName: '',
 
       /**
        * The tiers, grouped by world (old world and new world)
        */
       worlds: Tiers,
     };
+  },
+  computed: {
+    selectedWorld: function() {
+      const worlds = Object.values(this.worlds);
+      return worlds.find((world) => world.name === this.selectedWorldName);
+    },
   },
 };
 </script>
