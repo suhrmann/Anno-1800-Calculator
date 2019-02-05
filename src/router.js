a5c47001b6e56d4f0f9d81abb4223c1769db83e5
Simon Uhrmann <suhrmann@posteo.de>
2019-02-05 10:12:25 +0100

    Registered components in routes and updated routes in drawer accordingly

--- src/router.js
+++ src/router.js
@@ -1,13 +1,43 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+// Routes
 import ColorComponent from './components/ColorComponent.vue';
+import ProductionChains from './components/ProductionChains';
+import ProductionLayouts from './components/ProductionLayouts';
+import ResidentDemands from './components/ResidentDemands';
+import About from './components/About';
 
 Vue.use(Router);
 
 export default new Router({
-  routes: [{
-    path: '/colors',
-    name: 'colors',
-    component: ColorComponent,
-  }],
+  routes: [
+    // ------
+    {
+      path: '/colors',
+      name: 'colors',
+      component: ColorComponent,
+    },
+    // - Production ------
+    {
+      path: '/chains',
+      name: 'production-chains',
+      component: ProductionChains,
+    },
+    {
+      path: '/demands',
+      name: 'resident-demands',
+      component: ResidentDemands,
+    },
+    {
+      path: '/layouts',
+      name: 'production-layouts',
+      component: ProductionLayouts,
+    },
+    // ------
+    {
+      path: '/about',
+      name: 'about',
+      component: About,
+    },
+  ],
 });
