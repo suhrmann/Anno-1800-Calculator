abccad20c506e2f01852c0d7b4ae383e2241efb1
Simon Uhrmann <suhrmann@posteo.de>
2019-02-08 10:14:47 +0100

    Updated component `About`

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,20 +1,55 @@
 <template>
   <v-container>
+    <v-alert
+      :value="true"
+      type="secondary"
+      class="text-sm-center"
+    >
+      <p>
+        <strong> <span class="title">THANK YOU</span> Anno 1800 Wiki(s)!</strong>
+      </p>
+      If you want to improve this App contribute to the Wiki! This App is uses on the content, information and images of the Wiki.
+    </v-alert>
+
+    <p>&nbsp;</p>
+
     <h1>Author</h1>
-    Simon Uhrmann
+    <ul>
+      <li>Simon Uhrmann</li>
+      <li>Tobias Luderer</li>
+    </ul>
 
     <h3>Sources</h3>
-    The English Anno 1800 Wiki
-    <a href="http://de.anno1800.wikia.com/wiki/Anno_1800_Wiki">Anno 1800 Wiki [en]</a>
+    <ul>
+      <li>
+        The English Anno 1800 Wiki <br>
+        <a href="http://de.anno1800.wikia.com/wiki/Anno_1800_Wiki">Anno 1800 Wiki [en]</a>
+      </li>
+    </ul>
 
     The German Anno 1800 Wiki
     <a href="http://anno1800.wikia.com/wiki/Anno_1800_Wiki">Anno 1800 Wiki [de]</a>
 
-    <h3>Design</h3>
-    <a href="https://www.ubisoft.com/en-gb/game/anno-1800/">The official Anno 1800 page</a>
+    <h3>Images</h3>
+    <ul>
+      <li>
+        ALL Icons (buildings, population, items, etc.): <br>
+        <a href="http://anno1800.wikia.com/wiki/Anno_1800_Wiki">Anno 1800 Wiki [en]</a>
+      </li>
+      <li>
+        Background Images: <br>
+        <a href="https://store.ubi.com/de/anno-1800/5b647010ef3aa548048c5958.html">Anno 1800 in the Ubisoft Store</a>
+      </li>
+    </ul>
 
-    <h3>Background Images</h3>
-    <a href="https://store.ubi.com/de/anno-1800/5b647010ef3aa548048c5958.html">Anno 1800 in the Ubisoft Store</a>
+    <h3>Design</h3>
+    <ul>
+      <li>
+        Color scheme (based on): <br>
+        <a href="https://www.ubisoft.com/en-gb/game/anno-1800/">The official Anno 1800 page</a>
+      </li>
+    </ul>
+
   </v-container>
 </template>
 
