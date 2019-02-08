b461ed90ec127e5b7a9a52cf4d4f1512743d831d
Simon Uhrmann <suhrmann@posteo.de>
2019-02-08 09:44:35 +0100

    Download images grouped by population (farmer, worker, ...)  + check if directory exists  + improved filename formatting  + create directory if not exists

--- anno_1800_wiki_crawler/spiders/buildings.py
+++ anno_1800_wiki_crawler/spiders/buildings.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+import os
 import re
 import requests
 
@@ -13,26 +14,51 @@ class BuildingsSpider(scrapy.Spider):
     allowed_domains = ['anno1800.wikia.com', 'vignette.wikia.nocookie.net']
     start_urls = ['http://anno1800.wikia.com/wiki/Buildings']
 
+    # Store the downloaded images here
+    IMAGE_PATH = './images'
+
     # Define the regex we'll need to filter the returned links
     # Sample: https://vignette.wikia.nocookie.net/anno1800/images/a/a0/Workforce_-_farmers.png/revision/latest?cb=20190203165406
     url_matcher = re.compile('^https:\/\/vignette\.wikia\.nocookie\.net\/anno1800\/images\/')
 
-    # Create a set that'll keep track of ids we've crawled
+    # The XPaths to the various populations
+    POPULATION_XPATHS = {
+            # Old world:
+                'farmers':   '//*[@id="mw-content-text"]/table[1]',
+                'workers':   '//*[@id="mw-content-text"]/table[2]',
+                'artisans':  '//*[@id="mw-content-text"]/table[3]',
+                'engineers': '//*[@id="mw-content-text"]/table[4]',
+                'investors': '//*[@id="mw-content-text"]/table[5]',
+            # New world:
+                'jornaleros': '//*[@id="mw-content-text"]/table[6]',
+                'obreros':    '//*[@id="mw-content-text"]/table[7]',
+             }
+
+    # Keep track of the crawled ids
     crawled_images = []
 
     def parse(self, response):
-        body = Selector(text=response.body)
-        link_extractor = LinkExtractor(allow=BuildingsSpider.url_matcher)
-        image_urls = [link.url for link in link_extractor.extract_links(response)]
 
-        for image_url in image_urls:
-            filename = image_url.split('/')[-3]
-            file_path = './images/' + filename
-            self.download(image_url, file_path)
+        for key, xpath in self.POPULATION_XPATHS.items():
+            link_extractor = LinkExtractor(allow=BuildingsSpider.url_matcher, restrict_xpaths=xpath)
+            image_urls = [link.url for link in link_extractor.extract_links(response)]
+
+            # Check and create directory
+            path = os.path.join(self.IMAGE_PATH, key)
+            if not os.path.exists(path):
+                os.makedirs(path)
+
+            # Find and download all images
+            for image_url in image_urls:
+                filename = image_url.split('/')[-3]  # Extract the filename from URL
+                f_filename = self.format_filename(filename)  # The formatted filename
+                file_path = os.path.join(path, f_filename)  # The full path to store the file
+                self.download(image_url, file_path)  # Download the file
 
 
     def download(self, url, filename):
         """
+        !!! MAKE SURE THE DOWNLOAD DIRECTORY EXISTS !!!
         Download the URL to filename.
         :param url: The URL to download.
         :param filename: The filename and path of the downloaded file
@@ -52,11 +78,14 @@ class BuildingsSpider(scrapy.Spider):
         :return: The formatted new filename
         """
         # Formatting:
-        #  - all lowercase
-        #  - all hyphens, no lower dash
+        #  - lowercase
+        #  - hyphens instead of lower dash
         #  - remove spaces (in URLs: %27)
+        #  - replace file extension: Images are .webp instead of .png
         #  - remove ugly parts ("---", etc.)
         return filename\
             .lower()\
-            .replace('_', '-')\
-            .replace('-0', '').replace('%27', '').replace('---', '-').replace('--', '-')
+            .replace('_', '-') \
+            .replace('%27', '') \
+            .replace('.png', '.webp') \
+            .replace('-0', '').replace('---', '-').replace('--', '-')
