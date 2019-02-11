0dda21f3d16b9e468b59c9dc411500a10215721c
Simon Uhrmann <suhrmann@posteo.de>
2019-02-11 15:16:01 +0100

    Working bottom navigation bar - with images!!!  + prototype of production chains

--- src/components/production_chains/VisualProductionChain.vue
+++ src/components/production_chains/VisualProductionChain.vue
@@ -1,36 +1,22 @@
 <template>
   <v-container grid-list-md text-xs-center>
-    <v-layout row wrap>
+    <v-layout justify-center row wrap>
       <v-flex xs12>
-        <h2>Dynamic Grid TODO</h2>
-        <p>Selected Chain: {{productionChain}}</p>
-        <v-card dark color="primary">
-          <v-card-text class="px-0">12</v-card-text>
-        </v-card>
+        <h2>Dynamic Grid TODO1</h2>
+        <v-btn @click="test">test</v-btn>
+        <p>Selected Chain: {{productionChain.name}}</p>
+        <p>Grid Width: {{gridWidth}}</p>
+        <p>Grid Height: {{gridHeight}}</p>
       </v-flex>
-      <v-flex v-for="i in 2" :key="`6${i}`" xs6>
+    </v-layout>
+
+    <v-layout v-for="j in gridHeight" :key="j" justify-center row wrap>
+      <v-flex v-for="i in gridWidth" :key="i" xs1>
         <v-card dark color="secondary">
-          <v-card-text class="px-0">6</v-card-text>
-        </v-card>
-      </v-flex>
-      <v-flex v-for="i in 3" :key="`4${i}`" xs4>
-        <v-card dark color="primary">
-          <v-card-text class="px-0">4</v-card-text>
-        </v-card>
-      </v-flex>
-      <v-flex v-for="i in 4" :key="`3${i}`" xs3>
-        <v-card dark color="secondary">
-          <v-card-text class="px-0">3</v-card-text>
-        </v-card>
-      </v-flex>
-      <v-flex v-for="i in 6" :key="`2${i}`" xs2>
-        <v-card dark color="primary">
-          <v-card-text class="px-0">2</v-card-text>
-        </v-card>
-      </v-flex>
-      <v-flex v-for="i in 12" :key="`1${i}`" xs1>
-        <v-card dark color="secondary">
-          <v-card-text class="px-0">1</v-card-text>
+          <v-tooltip bottom>
+            <img slot="activator" :src="building.img" :alt="'building'">
+            <span>Building name</span>
+          </v-tooltip>
         </v-card>
       </v-flex>
     </v-layout>
@@ -38,10 +24,183 @@
 </template>
 
 <script>
+import { chainNodeMixin } from "./chainNodeMixin.js";
+import { EventBus } from "../../EventBus.js";
+
 export default {
+  data() {
+    return {
+      building: {
+        img: "test"
+      },
+
+      positionTree: {
+        depth: {
+          root: { sibling: {} },
+          parent: { sibling: {} },
+          grandparent: { sibling: {} },
+          greatGrandparent: { sibling: {} },
+          twoGreatGrandparent: { sibling: {} }
+        }
+      }
+    };
+  },
+
+  created() {
+    EventBus.$on("bottomNavBarChanged", () => {
+      this.resetPositionTree();
+      let chainNodeMixin = this;
+
+      let gridDimensions = JSON.parse(
+        JSON.stringify(this.makeMatchingGrid(this.gridHeight, this.gridWidth))
+      );
+
+      let gridToRender = this.fillGridWithProductionChain(
+        gridDimensions,
+        this.productionChain
+      );
+    });
+  },
+
+  mixins: [chainNodeMixin],
   computed: {
     productionChain() {
       return this.$store.state.selectedProductionChain;
+    },
+    gridWidth() {
+      let chainNodeMixin = this;
+      return chainNodeMixin.chainDepth * 2 + 1;
+    },
+    gridHeight() {
+      let chainNodeMixin = this;
+      return chainNodeMixin.chainWidth * 2 + 1;
+    }
+  },
+  methods: {
+    resetPositionTree() {
+      this.positionTree = {
+        depth: {
+          root: { sibling: {} },
+          parent: { sibling: {} },
+          grandparent: { sibling: {} },
+          greatGrandparent: { sibling: {} },
+          twoGreatGrandparent: { sibling: {} }
+        }
+      };
+    },
+    even(rowOrColumn) {
+      if (rowOrColumn % 2 == 0) return true;
+    },
+
+    /**
+     * This function takes a certain width and heigth and creates a 2-dimensional array
+     * @param {int} height
+     * @param {int} width
+     * @return {Array[height][width]}
+     */
+    makeMatchingGrid(height, width) {
+      let val = {};
+      let arr = [];
+      for (let i = 0; i < width; i++) {
+        arr[i] = [];
+        for (let j = 0; j < height; j++) {
+          arr[i][j] = val;
+        }
+      }
+
+      return arr;
+    },
+
+    /**
+     * This function takes a proper grid and integrates the production chain in the way it should be rendered
+     * @param {Array [][] } grid
+     * @param {Object} productionChain
+     * @return {Array [][] } An Array containing the production chain grid ready to render
+     */
+    fillGridWithProductionChain(grid, productionChain) {
+      // some attributes of the given grid
+      let arrayHeight = grid[0].length;
+      let arrayWidth = grid.length;
+
+      let lastObjectPosition = { x: arrayWidth - 1, y: arrayHeight / 2 + 0.5 };
+      let nextObjectPosition = { x: 0, y1: 0, y2: 0 };
+
+      console.log("Chain: " + productionChain.name);
+      console.log("GridHeight: " + arrayHeight);
+      console.log("GridWidth: " + arrayWidth);
+      console.log(
+        "Position Last: x:" +
+          lastObjectPosition.x +
+          " y:" +
+          lastObjectPosition.y
+      );
+
+      let chainNodeMixin = this;
+
+      chainNodeMixin.iterateProductionChain(
+        productionChain,
+        root => {
+          console.log("rootcallback");
+          const chainLevel = this.getCurrentPositionLevel(
+            this.chainDepthCounter
+          );
+          const siblingNumber = this.getCountedSiblingsUntil(
+            this.chainDepthArray,
+            this.chainDepthCounter
+          );
+
+          console.log(chainLevel, siblingNumber);
+
+          this.positionTree.depth[chainLevel].sibling[siblingNumber] = root;
+        },
+
+        element => {
+          console.log("elementcallback");
+
+          const chainLevel = this.getCurrentPositionLevel(
+            this.chainDepthCounter
+          );
+          const siblingNumber = this.getCountedSiblingsUntil(
+            this.chainDepthArray,
+            this.chainDepthCounter
+          );
+
+          console.log(chainLevel, siblingNumber);
+
+          this.positionTree.depth[chainLevel].sibling[siblingNumber] = element;
+        },
+        true
+      );
+      console.log("");
+      console.log("Visual Grid");
+      console.table(grid);
+      console.log("");
+      console.log("Elements sorted by position");
+      console.table(this.positionTree);
+    },
+
+    determineElementY() {
+      return 1;
+    },
+
+    determineElementX(lastObjectPosition) {
+      return lastObjectPosition.x - (this.chainDepthCounter - 1) * 2;
+    },
+
+    buildWidthGrid(element) {},
+
+    test() {
+      let chainNodeMixin = this;
+      let depth = chainNodeMixin.iterateProductionChain(
+        this.productionChain,
+        element => {
+          console.log("root callback gets executed!");
+        },
+        element => {
+          console.log("element callback gets executed!");
+        },
+        true
+      );
     }
   }
 };
@@ -49,3 +208,5 @@ export default {
 
 <style>
 </style>
+
+     
---
+++ src/components/production_chains/chainNodeMixin.js
@@ -0,0 +1,282 @@
+export const chainNodeMixin = {
+  name: 'ProductionChainNode',
+
+  data() {
+    return {
+      chainDepth: 0,
+      chainDepthCounter: 0,
+      chainDepthArray: [],
+
+      chainWidth: 0,
+      chainWidthCounter: 0,
+      chainWidthArray: [],
+
+      currentPosition: {
+        root: 0,
+        parent: 0,
+        grandparent: 0,
+        greatGrandparent: 0,
+        twoGreatGrandparent: 0,
+      },
+    };
+  },
+
+  methods: {
+
+    /**
+     * @param {int} to A Number to which the position is set
+     * @param {string} level The key of the currentPosition property to change
+     * @return {void} changes this.currentPosition.root
+     */
+    changePosition(to, level) {
+      this.currentPosition[level] = to;
+    },
+
+    /**
+     * @param {boolean} all Set true if you want to reset all keys to 0
+     * @param {string} level Set this key of the currentPosition property to 0
+     * @return {void} changes this.currentPosition.root
+     */
+    resetPosition(all, level) {
+      if (all) {
+        this.currentPosition = {
+          root: 0,
+          parent: 0,
+          grandparent: 0,
+          greatGrandparent: 0,
+          twoGreatGrandparent: 0,
+        };
+      } else if (level) {
+        this.currentPosition[level] = 0;
+      }
+    },
+
+
+    /**
+     * @param {int} chainDepth The element's level in the tree
+     * @return {String} the corresponding position key name
+     * Returns the key name of current depth Position
+     */
+    getCurrentPositionLevel(chainDepth) {
+      switch (chainDepth) {
+        case 1:
+          return 'root';
+        case 2:
+          return 'parent';
+        case 3:
+          return 'grandparent';
+        case 4:
+          return 'greatGrandparent';
+        case 5:
+          return 'twoGreatGrandparent';
+      }
+    },
+
+    /**
+     * @param {Array} chainDepthArray An Array filled with all objects so far stored tree level
+     * @param {int} chainDepth The element 's level in the tree
+     * @return {int} The current number of objects already in the given level
+     * -
+     * Counts the number of elements already stored for this depth level.
+     * Think: There are already 2 elements stored on this depth, so this must be sibling three
+     */
+    getCountedSiblingsUntil(chainDepthArray, chainDepth) {
+      let count = 0;
+      const arr = chainDepthArray;
+      for (let i = 0; i < arr.length; ++i) {
+        if (arr[i] === chainDepth) {
+          count++;
+        }
+      }
+      return count;
+    },
+
+
+    /**
+     * Determines the maximum width of the chain
+     * @return {int} the tree width
+     */
+    determineChainWidth() {
+      const depthArray = JSON.parse(JSON.stringify(this.chainDepthArray));
+      const counts = {};
+      const countsArray = [];
+
+      for (let i = 0; i < depthArray.length; i++) {
+        counts[depthArray[i]] = (counts[depthArray[i]] + 1) || 1;
+      }
+
+      Object.keys(counts).forEach((depthLevel) => {
+        countsArray.push(counts[depthLevel]);
+      });
+
+      const treeWidth = Math.max(...countsArray);
+      return treeWidth;
+    },
+
+    /**
+     * @param {Object} productionChain a variable containing a ProductionChain Object
+     * @param {function} rootCallbackFunction a function which only gets execeuted on the root element
+     * @param {function} elementCallbackFunction a function which gets executed on every tree element
+     * which is not the root. The element is given to the callback function as a parameter
+     * @param {boolean} debugOutput if true, debug output gets written in the console
+     * @return {void} the depth of the Production Chain and the callback function's
+     * result can be accessed by a vdata property. Recommendation: populate an Object or Array with the results
+     * ---
+     * Iterates through all tree objects and executes a function at every element.
+     * Determines the Production Chain's depth.
+     */
+    iterateProductionChain(productionChain, rootCallbackFunction, elementCallbackFunction, debugOutput) {
+      this.chainDepthArray = []; // this array stores the depth levels on every element
+      this.chainDepthCounter = 0;
+      this.resetPosition(true);
+      const root = productionChain; // naming reasons
+
+      this.changePosition(1, 'root');
+
+      if (debugOutput) {
+        console.log('');
+        console.log('---------------------------------');
+        console.log('Chain: ' + productionChain.name);
+        console.log('Position: ');
+        console.log(this.currentPosition);
+      }
+
+
+      // if the root element is already a leaf, execute callback function and return depth 1
+      // if not, recursively iterate through the tree
+      if (!this.isLeaf(root)) {
+        // needs to be incremented and set to ensure that the correct data is given when the root function is exevuted
+        this.chainDepthCounter++;
+        this.chainDepthArray.push(this.chainDepthCounter);
+        if (rootCallbackFunction) rootCallbackFunction(root, this.currentPosition);
+        this.chainDepthCounter--;
+        this.chainDepthArray = [];
+
+
+        this.iterateTreeElements(root, elementCallbackFunction, debugOutput);
+      } else {
+        this.chainDepth = 1;
+        this.chainWidth = 1;
+        this.chainDepthArray = [1];
+        if (rootCallbackFunction) rootCallbackFunction(root, this.currentPosition);
+      }
+
+      // determine the highest number in chainDepthArray, which is the tree height
+      this.chainDepth = Math.max(...this.chainDepthArray);
+      this.chainWidth = this.determineChainWidth();
+
+      if (debugOutput) {
+        console.log('');
+        console.log('Array: ' + this.chainDepthArray);
+        console.log('Chain Depth: ' + this.chainDepth);
+        console.log('Chain Width: ' + this.chainWidth);
+      }
+    },
+
+    /**
+     * @param {Object} element a variable containing an element of the production chain
+     * @param {function} elementCallbackFunction a function which gets executed on every tree element
+     * @param {boolean} debugOutput if true, debug output gets written in the console
+     * ---
+     * If the given element is the root or a node:
+     * raise the depth counter by 1
+     * push the current depth to chainDepthArray
+     * call iteration on all precursors
+     * ---
+     * If the given element is a leaf:
+     * raise the depth counter by 1
+     * push the current depth to chainDepthArray
+     * ---
+     * If the procedure has reached a leaf, the depth counter
+     * gets decremented by 1 at each ndde "on its way back"
+     * if the procedure reaches a node on its way back,
+     * the iteration continues on the other precursors incrementing the counter again
+     */
+    iterateTreeElements(element, elementCallbackFunction, debugOutput) {
+      if (debugOutput) {
+        console.log('');
+        console.log('Element: "' + element.building + '", Leaf: ' + this.isLeaf(element));
+      }
+      if (!this.isLeaf(element)) {
+        // if element is root or node
+        this.chainDepthCounter++;
+        this.chainDepthArray.push(this.chainDepthCounter);
+
+        const chainLevel = this.getCurrentPositionLevel(this.chainDepthCounter);
+        const siblingNumber = this.getCountedSiblingsUntil(this.chainDepthArray, this.chainDepthCounter);
+
+        if (debugOutput) {
+          console.log('DepthPosition: ' + chainLevel);
+          console.log('Sibling Number: ' + siblingNumber);
+        }
+
+        this.changePosition(siblingNumber.chainLevel);
+
+
+        // only execute callback function when element != root
+        if (this.chainDepthCounter !== 1) {
+          if (elementCallbackFunction) elementCallbackFunction(element, this.currentPosition);
+        }
+
+        if (debugOutput) {
+          console.log('Element Depth: ' + this.chainDepthCounter);
+        }
+
+        // recursively iterat        switch(this.chainDepthCounter)e on precursors
+        for (const precursors in element.precursorBuildings) {
+          if (precursors) {
+            const precursor = element.precursorBuildings[precursors];
+            this.iterateTreeElements(precursor, elementCallbackFunction, debugOutput);
+          }
+        }
+      } else {
+        // if element is a leaf
+        this.chainDepthCounter++;
+        this.chainDepthArray.push(this.chainDepthCounter);
+
+        const chainLevel = this.getCurrentPositionLevel(this.chainDepthCounter);
+        const siblingNumber = this.getCountedSiblingsUntil(this.chainDepthArray, this.chainDepthCounter);
+
+        if (debugOutput) {
+          console.log('DepthPosition: ' + chainLevel);
+          console.log('Sibling Number: ' + siblingNumber);
+        }
+
+        this.changePosition(siblingNumber.chainLevel);
+
+        // only execute callback function when element != root
+        if (this.chainDepthCounter !== 1) {
+          if (elementCallbackFunction) elementCallbackFunction(element, this.currentPosition);
+        }
+
+        if (debugOutput) {
+          console.log('Element Depth: ' + this.chainDepthCounter);
+        }
+      }
+      // execute when going "up" the tree
+      this.chainDepthCounter--;
+    },
+
+
+    /**
+     * @param {Object} element a production chain element
+     * @return {int} the number of precursors of the given lement
+     */
+    countPrecursors(element) {
+      return Object.keys(element.precursorBuildings).length;
+    },
+
+    /**
+     * Is this node a leaf (= this node has no next nodes)?
+     * @param {Object} element Root, node or leaf object
+     * @return {boolean} true if this node has no next nodes.
+     */
+    isLeaf(element) {
+      if (Object.keys(element.precursorBuildings).length === 0) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+};
