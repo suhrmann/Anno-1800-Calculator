e75980770d2b70707eeb15cdf906d43d9169cf5f
Tobias Luderer <tobias.luderer@gmail.com>
2019-04-26 17:37:57 +0200

    changed router params to query in demands<->chains

--- src/components/ResidentDemands.vue
+++ src/components/ResidentDemands.vue
@@ -8,85 +8,56 @@
       <bottom-population-input></bottom-population-input>
 
       <v-container grid-list-md text-xs-center>
-
         <!-- Alert if no population was entered -->
         <div v-if="isPopulationEmpty">
-          <v-alert
-            class="my-5"
-            :value="true"
-            type="warning"
-            transition="scale-transition"
-          >
-            <h3>No population set</h3>
-            Enter the number of at least one populations to start calculation of demands.
+          <v-alert class="my-5" :value="true" type="warning" transition="scale-transition">
+            <h3>No population set</h3>Enter the number of at least one populations to start calculation of demands.
           </v-alert>
         </div>
 
         <!-- Demands calculaiton -->
-        <v-tabs
-          v-model="activeTab"
-          color="primary"
-          slider-color="secondary"
-          dark
-          fixed-tabs
-        >
+        <v-tabs v-model="activeTab" color="primary" slider-color="secondary" dark fixed-tabs>
           <!-- TAB: Demands as cards -->
-          <v-tab
-            :key="1"
-          >
-            Demands Cards
-          </v-tab>
-          <v-tab-item
-            :key="1"
-          >
+          <v-tab :key="1">Demands Cards</v-tab>
+          <v-tab-item :key="1">
             <v-card flat>
               <resident-demands-cards></resident-demands-cards>
             </v-card>
           </v-tab-item>
 
           <!-- TAB: Demands as table -->
-          <v-tab
-            :key="2"
-          >
-            Demands Table
-          </v-tab>
-          <v-tab-item
-            :key="2"
-          >
+          <v-tab :key="2">Demands Table</v-tab>
+          <v-tab-item :key="2">
             <v-card flat>
               <resident-demands-table></resident-demands-table>
             </v-card>
           </v-tab-item>
-
         </v-tabs>
-
       </v-container>
-
     </v-layout>
   </v-container>
 </template>
 
 <script>
-import BottomPopulationInput from './resident_demands/BottomPopulationInput';
-import ResidentDemandsCards from './resident_demands/ResidentDemandsCards';
-import ResidentDemandsTable from './resident_demands/ResidentDemandsTable';
-import residentDemandCalculatorMixin from './resident_demands/residentDemandCalculatorMixin.js';
+import BottomPopulationInput from "./resident_demands/BottomPopulationInput";
+import ResidentDemandsCards from "./resident_demands/ResidentDemandsCards";
+import ResidentDemandsTable from "./resident_demands/ResidentDemandsTable";
+import residentDemandCalculatorMixin from "./resident_demands/residentDemandCalculatorMixin.js";
 
 export default {
-  name: 'ResidentDemands',
+  name: "ResidentDemands",
   components: {
-    'resident-demands-cards': ResidentDemandsCards,
-    'resident-demands-table': ResidentDemandsTable,
+    "resident-demands-cards": ResidentDemandsCards,
+    "resident-demands-table": ResidentDemandsTable,
 
-    'bottom-population-input': BottomPopulationInput,
+    "bottom-population-input": BottomPopulationInput
   },
 
   beforeRouteEnter(to, from, next) {
     // do sth
-    next((vm) => {
-      if(vm.$route.params.linkedFromChains === true)
-      {
-        let newPop = vm.$route.params.populationToAdd
+    next(vm => {
+      if (vm.$route.query.linkedFromChains === true) {
+        let newPop = vm.$route.query.populationToAdd;
         vm.$store.commit("addToPopulationDemands", newPop);
       }
     });
@@ -95,8 +66,7 @@ export default {
   mixins: [residentDemandCalculatorMixin],
 
   data: function() {
-    return {
-    };
+    return {};
   },
 
   computed: {
@@ -105,28 +75,29 @@ export default {
         return this.$store.state.selectedConsumptionTab;
       },
       set: function(tabID) {
-        this.$store.commit('changeSelectedConsumptionTab', tabID);
-      },
+        this.$store.commit("changeSelectedConsumptionTab", tabID);
+      }
     },
 
     /**
-       * Check if any population was entered.
-       *
-       * @return {boolean} true if no population is set.
-       */
+     * Check if any population was entered.
+     *
+     * @return {boolean} true if no population is set.
+     */
     isPopulationEmpty: function() {
-      return !(this.$store.state.population.numFarmers > 0 ||
-          this.$store.state.population.numWorkers > 0 ||
-          this.$store.state.population.numArtisans > 0 ||
-          this.$store.state.population.numEngineers > 0 ||
-          this.$store.state.population.numInvestors > 0 ||
-          this.$store.state.population.numJornaleros > 0 ||
-          this.$store.state.population.numObreros > 0);
-    },
-  },
+      return !(
+        this.$store.state.population.numFarmers > 0 ||
+        this.$store.state.population.numWorkers > 0 ||
+        this.$store.state.population.numArtisans > 0 ||
+        this.$store.state.population.numEngineers > 0 ||
+        this.$store.state.population.numInvestors > 0 ||
+        this.$store.state.population.numJornaleros > 0 ||
+        this.$store.state.population.numObreros > 0
+      );
+    }
+  }
 };
 </script>
 
 <style scoped>
-
 </style>
