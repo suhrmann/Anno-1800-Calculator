1df8e73fa754810a4d881546f445fb93df925fc7
Simon Uhrmann <suhrmann@posteo.de>
2019-05-13 16:29:00 +0200

    Configure ``vuex-persistedstate`` to store  * selected production chain  * entered population

--- src/store.js
+++ src/store.js
@@ -5,7 +5,32 @@ import createPersistedState from 'vuex-persistedstate';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  plugins: [createPersistedState()],
+  plugins: [
+    // Configure the plugin "vuex-persistedstate" (key and what to store)
+    createPersistedState({
+      /**
+       * The key to store the persisted state under. (default: vuex)
+       */
+      key: 'anno-1800-caluclator',
+      /**
+       * reducer?: (state: any, paths: string[]) => object;
+       *
+       * A function that will be called to reduce the state to persist based on the given paths.
+       * Defaults to include the values.
+       */
+      reducer(state, paths) {
+        return {
+          // Store selected production chain
+          selectedWorldID: state.selectedWorldID,
+          selectedSocialClassID: state.selectedSocialClassID,
+          selectedProductionChainID: state.selectedProductionChainID,
+
+          // Store entered population
+          population: state.population,
+        };
+      },
+    }),
+  ],
 
   state: {
     selectedProductionChain: null,
