4c2b13df3b001ec0bafe4b5574b990ab90ddcdb7
Simon Uhrmann <suhrmann@posteo.de>
2019-05-13 14:02:25 +0200

    ESLint

--- src/components/resident_demands/residentDemandCalculatorMixin.js
+++ src/components/resident_demands/residentDemandCalculatorMixin.js
@@ -2,16 +2,16 @@ import Consumption from '../../data/consumption.json';
 import Producers from '../../data/producers.json';
 import NonProducers from '../../data/non-producers.json';
 import {
-  chainNodeMixin
+  chainNodeMixin,
 } from '../production_chains/chainNodeMixin';
 import {
-  helperFunctionMixin
+  helperFunctionMixin,
 } from '../helperFunctionMixin';
 
 export default {
   name: 'ResidentDemandCalculator',
   mixins: [chainNodeMixin, helperFunctionMixin],
-  data: function () {
+  data: function() {
     return {
       producers: Producers.Producers,
       nonProducers: NonProducers.buildings,
@@ -20,73 +20,73 @@ export default {
   },
   computed: {
     // Computed properties for Vuex values
-    numFarmers: function () {
+    numFarmers: function() {
       return this.$store.state.population.numFarmers;
     },
-    numWorkers: function () {
+    numWorkers: function() {
       return this.$store.state.population.numWorkers;
     },
-    numArtisans: function () {
+    numArtisans: function() {
       return this.$store.state.population.numArtisans;
     },
-    numEngineers: function () {
+    numEngineers: function() {
       return this.$store.state.population.numEngineers;
     },
-    numInvestors: function () {
+    numInvestors: function() {
       return this.$store.state.population.numInvestors;
     },
-    numJornaleros: function () {
+    numJornaleros: function() {
       return this.$store.state.population.numJornaleros;
     },
-    numObreros: function () {
+    numObreros: function() {
       return this.$store.state.population.numObreros;
     },
 
 
     // Compute population demands
-    farmersDemands: function () {
+    farmersDemands: function() {
       const farmersDemands = this.consumption.Consumption.farmers;
       return {
         basic: this.calculateDemands(farmersDemands.basic, this.numFarmers),
         luxury: this.calculateDemands(farmersDemands.luxury, this.numFarmers),
       };
     },
-    workersDemands: function () {
+    workersDemands: function() {
       const workersDemands = this.consumption.Consumption.workers;
       return {
         basic: this.calculateDemands(workersDemands.basic, this.numWorkers),
         luxury: this.calculateDemands(workersDemands.luxury, this.numWorkers),
       };
     },
-    artisansDemands: function () {
+    artisansDemands: function() {
       const artisansDemands = this.consumption.Consumption.artisans;
       return {
         basic: this.calculateDemands(artisansDemands.basic, this.numArtisans),
         luxury: this.calculateDemands(artisansDemands.luxury, this.numArtisans),
       };
     },
-    engineersDemands: function () {
+    engineersDemands: function() {
       const engineersDemands = this.consumption.Consumption.engineers;
       return {
         basic: this.calculateDemands(engineersDemands.basic, this.numEngineers),
         luxury: this.calculateDemands(engineersDemands.luxury, this.numEngineers),
       };
     },
-    investorsDemands: function () {
+    investorsDemands: function() {
       const investorsDemands = this.consumption.Consumption.investors;
       return {
         basic: this.calculateDemands(investorsDemands.basic, this.numInvestors),
         luxury: this.calculateDemands(investorsDemands.luxury, this.numInvestors),
       };
     },
-    jornalerosDemands: function () {
+    jornalerosDemands: function() {
       const jornalerosDemands = this.consumption.Consumption.jornaleros;
       return {
         basic: this.calculateDemands(jornalerosDemands.basic, this.numJornaleros),
         luxury: this.calculateDemands(jornalerosDemands.luxury, this.numJornaleros),
       };
     },
-    obrerosDemands: function () {
+    obrerosDemands: function() {
       const obrerosDemands = this.consumption.Consumption.obreros;
       return {
         basic: this.calculateDemands(obrerosDemands.basic, this.numObreros),
@@ -109,7 +109,7 @@ export default {
      *           }
      *         }
      */
-    totalDemands: function () {
+    totalDemands: function() {
       // Merge all demands
       const demands = {
         farmers: this.farmersDemands,
@@ -123,7 +123,7 @@ export default {
 
       const totalDemands = {
         basic: {},
-        luxury: {}
+        luxury: {},
       };
       // Iterate over all populations
       for (const [pKey, population] of Object.entries(demands)) { // eslint-disable-line no-unused-vars
@@ -156,7 +156,7 @@ export default {
      * @param {object} newConsumption
      * @param {object} oldConsumption Unused.
      */
-    totalDemands: function (newConsumption, oldConsumption) {
+    totalDemands: function(newConsumption, oldConsumption) {
       this.$store.commit('setConsumption', newConsumption);
     },
   },
@@ -188,7 +188,7 @@ export default {
     getImage(image, folder) {
       return image ? require(`../../assets/${folder}/${image}`) : '';
     },
-    calculateDemands: function (populationDemands, numPopulation) {
+    calculateDemands: function(populationDemands, numPopulation) {
       const demands = {};
       for (const [key, demand] of Object.entries(populationDemands)) {
         if (demand) {
@@ -217,7 +217,7 @@ export default {
      * @param {float} usage The number of usage to format.
      * @return {string} The pretty formatted usage.
      */
-    formatUsage: function (usage) {
+    formatUsage: function(usage) {
       return Math.round(usage * 100000) / 100000;
     },
 
@@ -232,11 +232,11 @@ export default {
       const socialClass = helperFunctionMixin.getSocialClassByID(selectedChain.socialClassID);
       const world = helperFunctionMixin.getWorldByID(socialClass.worldID);
       this.$store.commit(
-        'changeSelectionIDs', {
-          worldID: world.id,
-          socialClassID: socialClass.id,
-          chainID: selectedChain.id,
-        });
+          'changeSelectionIDs', {
+            worldID: world.id,
+            socialClassID: socialClass.id,
+            chainID: selectedChain.id,
+          });
       this.$store.commit('changeProductionChain', selectedChain);
       this.$router.push('/chains');
     },
