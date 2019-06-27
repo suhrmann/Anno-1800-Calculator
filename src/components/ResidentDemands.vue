d17af66e0aecbcc385f986d23e2c58593ef0a665
Simon Uhrmann <suhrmann@posteo.de>
2019-06-27 12:21:52 +0200

    Added option to enter number of population by the amount of residences to address #18  + Refactoring: Renamed component `BottomPopulationInput.vue`

--- src/components/ResidentDemands.vue
+++ src/components/ResidentDemands.vue
@@ -5,7 +5,72 @@
         <h1>Resident Demands</h1>
       </v-flex>
       <!-- Resident Demands -->
-      <bottom-population-input></bottom-population-input>
+
+      <v-flex
+        xs12
+      >
+
+<!--
+        <v-alert
+          :value="true"
+          color="secondary"
+          icon="mdi-information"
+          outline
+          dismissible
+        >
+          Enter your population either directly as number of <kbd>Population</kbd> (farmers, workers, etc.) or via the amount of
+          <kbd>Residences</kbd> per population.
+        </v-alert>
+-->
+
+        <v-tabs
+          centered
+          color="primary"
+          dark
+          icons-and-text
+          fixed-tabs
+          grow
+        >
+          <v-tabs-slider color="secondary"></v-tabs-slider>
+
+          <!-- Population input -->
+          <v-tab>
+            Population
+            <v-avatar
+              color="grey lighten-2"
+            >
+              <img src="../assets/population/engineers.webp" alt="avatar">
+            </v-avatar>
+          </v-tab>
+          <v-tab-item
+            key="1"
+          >
+            <v-card flat>
+              <v-card-text>
+                <population-input></population-input>
+              </v-card-text>
+            </v-card>
+          </v-tab-item>
+
+          <!-- Residence input -->
+          <v-tab>
+            Residence
+            <v-avatar>
+              <img src="../assets/buildings/farmers/residence.webp" alt="avatar">
+            </v-avatar>
+          </v-tab>
+          <v-tab-item
+            key="2"
+          >
+            <v-card flat>
+              <v-card-text>
+                <residence-input></residence-input>
+              </v-card-text>
+            </v-card>
+          </v-tab-item>
+
+        </v-tabs>
+      </v-flex>
 
       <v-container grid-list-md text-xs-center>
 
@@ -18,7 +83,8 @@
 </template>
 
 <script>
-import BottomPopulationInput from './resident_demands/BottomPopulationInput';
+import PopulationInput from './resident_demands/PopulationInput';
+import ResidenceInput from './resident_demands/ResidenceInput';
 import ResidentDemandsTable from './resident_demands/ResidentDemandsTable';
 import residentDemandCalculatorMixin from './resident_demands/residentDemandCalculatorMixin.js';
 
@@ -27,7 +93,8 @@ export default {
   components: {
     'resident-demands-table': ResidentDemandsTable,
 
-    'bottom-population-input': BottomPopulationInput,
+    'population-input': PopulationInput,
+    'residence-input': ResidenceInput,
   },
 
   beforeRouteEnter(to, from, next) {
