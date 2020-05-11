ae4307322c44b9074a1b646e4bc3683383ab1bc8
Simon Uhrmann <suhrmann@posteo.de>
2020-05-11 17:31:20 +0200

    Make "Production Layout" full size without padding

--- src/components/ProductionLayouts.vue
+++ src/components/ProductionLayouts.vue
@@ -1,15 +1,18 @@
 <template>
-  <v-container fluid fill-height>
+  <v-container fluid fill-height pa-0>
     <v-layout wrap align-content-space-between fill-height>
       <v-flex xs12 id="frame">
+
+        <!-- Load Production Layouts from Anno 1800 wiki -->
         <iframe
           id="frame"
           referrerpolicy="no-referrer"
           allowfullscreen="true"
           src="https://anno1800.fandom.com/wiki/Production_layouts"
         ></iframe>
+
       </v-flex>
-      <!-- Production Layouts 
+      <!-- Production Layouts
       <bottom-nav-bar></bottom-nav-bar>
       -->
     </v-layout>
