1276ebe858febc61bb82a29c19742b80d5e90b8b
Simon Uhrmann <suhrmann@posteo.de>
2020-10-30 14:38:25 +0100

    Configure HTML title, injected by Webpack

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'Anno 1800 Calculator'
+        return args
+      })
+  }
 }
