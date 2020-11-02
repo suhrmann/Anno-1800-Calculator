121e9c52a617245a138e7a2020626fe77e7885b4
Simon Uhrmann <suhrmann@posteo.de>
2020-11-03 00:09:59 +0100

    Add icons to options

--- src/components/production_chains/VisualProductionChain.vue
+++ src/components/production_chains/VisualProductionChain.vue
@@ -5,11 +5,24 @@
         <v-card class="mb-3" color="secondary" dark tile>
           <v-card-title primary class="pb-0 title">Options</v-card-title>
           <v-card-text class="mb-0 pb-0 pt-0">
+            <!-- Option: Charcoal or Coal -->
             <v-radio-group class="pb-0 mb-0" v-model="coalOption" row>
-              <v-radio label="Charcoal" value="char" ></v-radio>
-              <v-radio label="Coal" value="rock"></v-radio>
+              <div style="width: 35px">&nbsp;</div>
+              <v-radio label="Charcoal" value="char" >
+                <template v-slot:label>
+                  <v-img src="@/assets/buildings/workers/charcoal-kiln.webp" contain width="32px"/> &nbsp;
+                  Charcoal
+                </template>
+              </v-radio>
+              <v-radio label="Coal" value="rock">
+                <template v-slot:label><v-img src="@/assets/buildings/artisans/coal.webp" contain width="32px"/>&nbsp;
+                  Coal
+                </template>
+              </v-radio>
             </v-radio-group>
+            <!-- Option: New World or Old World Marquetry -->
             <v-radio-group class="mb-0 pt-0 pt-0 mt-0" v-model="marquetryOption" row>
+              <div style="width: 35px"><v-img src="@/assets/buildings/investors/wood-veneers.webp" contain width="32px" height="32px" /></div>
               <v-radio label="Old World Marquetry" value="old"></v-radio>
               <v-radio label="New World Marquetry" value ="new"></v-radio>
             </v-radio-group>
@@ -399,4 +412,8 @@ h3,h4 {
 .v-expansion-panel-content > :first-child {
   padding: 0;
 }
+ .inline-img {
+   height: 66%;
+   width: auto;
+ }
 </style>
