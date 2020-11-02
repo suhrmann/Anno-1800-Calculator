99de0c376ff24c64e68ce322f5f6b9c726040cdd
Simon Uhrmann <suhrmann@posteo.de>
2020-11-02 20:14:50 +0100

    Update and add warning about unimplemented crawler for population

--- anno_1800_wiki_crawler/spiders/population.py
+++ anno_1800_wiki_crawler/spiders/population.py
@@ -4,8 +4,14 @@ import scrapy
 
 class PopulationSpider(scrapy.Spider):
     name = 'population'
-    allowed_domains = ['anno1800.wikia.com', 'vignette.wikia.nocookie.net']
-    start_urls = ['http://anno1800.wikia.com/wiki/Population']
+    allowed_domains = ['anno1800.wikia.com', 'anno1800.fandom.com', 'static.wikia.nocookie.net']
+    start_urls = ['https://anno1800.fandom.com/wiki/Population']
+
+    def __init__(self):
+        # TODO Remove this constructor when implemented!
+        self.logger.error("Population not implemented  yet!")
+        exit(1)
 
     def parse(self, response):
-        pass
+        self.logger.error("Population not implemented  yet!")
+        exit(1)
