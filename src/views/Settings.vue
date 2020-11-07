5fc35a9725bea58dd985ad7747a3321b7e71619a
Simon Uhrmann <suhrmann@posteo.de>
2020-11-07 02:17:27 +0100

    Extract DLC selection to own component !WIP!

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -12,49 +12,20 @@
         <h2>DLCs</h2>
       </v-col>
     </v-row>
-    <v-row align-content="middle">
-      <v-col
-        v-for="dlc in dlcs"
-        :key="dlc.id"
-        cols="2"
-        class="border"
-      >
-        <v-card
-          elevation="2"
-          tile
-          class="secondary"
-          dark
-        >
-          <v-card-text class="pa-2">
-            <v-checkbox
-              v-model="enabledDLCs"
-              :value="dlc.id"
-            >
-              <template slot="label">
-                <v-row><v-col>
-                  <v-img :src="require('@/assets/dlcs/' + dlc.img)"></v-img>
-                </v-col></v-row>
-                <v-row><v-col>
-                    Owned / Unowned
-                </v-col></v-row>
-              </template>
-            </v-checkbox>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+    <select-dlcs></select-dlcs>
   </v-container>
 </template>
 
 <script>
-import dlcs from '@/data/dlcs.json'
+import SelectDLCs from '@/components/settings/SelectDLCs'
 
 export default {
   name: 'Settings.vue',
+  components: {
+    'select-dlcs': SelectDLCs
+  },
   data: function () {
     return {
-      dlcs: dlcs,
-      enabledDLCs: []
     }
   }
 }
