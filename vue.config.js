fafa2c3200b221017ced0586cf0d9b777dcd7e40
Simon Uhrmann <suhrmann@posteo.de>
2021-01-01 23:23:36 +0100

    Add config for electron-builder + icon

--- vue.config.js
+++ vue.config.js
@@ -12,5 +12,28 @@ module.exports = {
         args[0].title = 'Anno 1800 Calculator'
         return args
       })
+  },
+
+  pluginOptions: {
+    /**
+     * Vue CLI Plugin Electron Builder
+     * See: https://nklayman.github.io/vue-cli-plugin-electron-builder/guide/configuration.html#configuration
+     */
+    electronBuilder: {
+      builderOptions: {
+        appId: 'com.github.anno-1800-calculator',
+        productName: 'Anno 1800 Calculator',
+        mac: {
+          category: 'public.app-category.games'
+        },
+        dmg: {
+          icon: false
+        },
+        linux: {
+          target: ['AppImage'],
+          category: 'Game'
+        }
+      }
+    }
   }
 }
