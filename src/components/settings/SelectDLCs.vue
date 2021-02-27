6efb11b957e1c3f0b5a3db1f8851d9d398a93f88
Cahllagerfeld <jkarl0710@gmail.com>
2021-02-27 11:55:22 +0100

    fix: :bug: Adjustment Layout for SelectDLC
    Fixed Layout for Select DLCs Component + making it responsive

--- src/components/settings/SelectDLCs.vue
+++ src/components/settings/SelectDLCs.vue
@@ -1,35 +1,23 @@
 <template>
-  <v-row align-content="middle">
-    <v-col
-      v-for="dlc in dlcs"
-      :key="dlc.id"
-      cols="2"
-      class="border"
-    >
-      <v-card
-        elevation="2"
-        tile
-        class="secondary"
-        dark
-      >
-        <v-card-text class="pa-2">
-          <v-checkbox
-            v-model="enabledDLCs"
-            :value="dlc.id"
-          >
-            <template slot="label">
-              <v-row><v-col>
-                <v-img :src="require('@/assets/dlcs/' + dlc.img)"></v-img>
-              </v-col></v-row>
-              <v-row><v-col>
-                Owned / Unowned
-              </v-col></v-row>
-            </template>
-          </v-checkbox>
-        </v-card-text>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col cols="12" sm="4" xs="1" lg="4" xl="3" v-for="dlc in dlcs" :key="dlc.id">
+          <v-card elevation="2" tile class="secondary card justify-center" dark>
+            <v-img class="card__image" :src="require('@/assets/dlcs/' + dlc.img)"></v-img>
+         <v-card-actions>
+            <v-checkbox
+              v-model="enabledDLCs"
+              :value="dlc.id"
+              :label="dlc.name"
+            >
+            </v-checkbox>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -46,6 +34,12 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss">
+.card {
+  height: auto;
+   padding:0.5rem;
+  &__image {
+    height: auto;
+  }
+}
 </style>
