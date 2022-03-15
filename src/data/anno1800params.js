dfafa3fec88d8e72269ee72f24ebbb1fa453de0c
Simon Uhrmann <simon.uhrmann@th-deg.de>
2022-03-15 10:36:27 +0100

    Add Vue-import wrapper for params.js

---
+++ src/data/anno1800params.js
@@ -0,0 +1,28 @@
+import { params } from './params'
+
+/**
+ * Wrapper for params.js
+ * Provides the Anno 1800 data from NiHoel's params.js for this calculator.
+ */
+
+export const factories = params.factories
+export const goodConsumptionUpgrades = params.goodConsumptionUpgrades
+export const icons = params.icons
+export const items = params.items
+export const languages = params.languages
+export const modules = params.modules
+export const newspaper = params.newspaper
+export const palaceBuffs = params.palaceBuffs
+export const populationGroups = params.populationGroups
+export const populationLevels = params.populationLevels
+export const powerPlants = params.powerPlants
+export const productFilter = params.productFilter
+export const products = params.products
+export const publicRecipeBuildings = params.publicRecipeBuildings
+export const recipeLists = params.recipeLists
+export const regions = params.regions
+export const residenceBuildings = params.residenceBuildings
+export const sessions = params.sessions
+export const tradeContracts = params.tradeContracts
+export const traders = params.traders
+export const workforce = params.workforce
