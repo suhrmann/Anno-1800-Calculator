3e1d92fd7e40525bf6c2710a20e7247bc654e8a2
Tobias Luderer <tobias.luderer@gmail.com>
2022-04-21 18:12:27 +0200

    expanded on chain builder

--- src/components/production_chains/factory.ts
+++ src/components/production_chains/factory.ts
@@ -1,38 +1,103 @@
 import { factories } from '../../data/anno1800params.js'
 import icons  from '../../data/anno1800icons.js' 
+import { Main } from 'electron';
 
 class Factory {
     id: number;
-    productionTime?: number;
+    productionTime: number
     iconPath?: string;
     maintenanceReq: Maintenance;
-    buildCost: BuildMaterial
+    buildCost: BuildCost
     inputs: Array<Factory>
+    isRootFactory: boolean
+    minimumProductionTimeOfChain: number = -1 //always -1 if not a root factory
+    minimumFactoryCount: number = 0
 
 
-    constructor(factoryID: number) {
-        const buildingIcons = [...icons.products.data, ...icons.factories.data]
-        this.id = factoryID
+    constructor(factoryID: number, isRoot: boolean = true) {
+        // get raw data
         const factoryInfo = factories.find((annoFactory: any) => annoFactory.guid === factoryID)
-        this.productionTime = factoryInfo?.tpmin;
+        const buildingIcons = [...icons.products.data, ...icons.factories.data]
+
+        // assign attributes
+        this.id = factoryID
+        this.productionTime = factoryInfo?.tpmin ?? 1000;
         this.iconPath = buildingIcons.find((buildingIcon: any) => buildingIcon.guid === factoryID)?.iconPath
         this.maintenanceReq = new Maintenance()
-        this.buildCost = new BuildMaterial()
+        this.buildCost = new BuildCost()
         this.inputs = []
+        this.isRootFactory = isRoot
 
+        // fill maintenanceReq object
         factoryInfo?.maintenances.forEach(maintenanceObject => {
             this.maintenanceReq.addByID(maintenanceObject.Product, maintenanceObject.Amount)
         })
 
+        // fill buildCost object
+        // DATA MISSING!
+        /* factoryInfo?.buildCost.forEach(buildCostObject => {
+            this.buildCost.addByID(buildCostObject.Product, buildCostObject.Amount)
+        }) */
+
         // recursive factory walk
-        // walks all possible factories, even those on another region
+        // walks all possible inputs, even those on another region and creates a Factory object for it.
+        // since we do this in the constructor, this operation also happens on every created input.
         factoryInfo?.inputs?.forEach(input => {
             const precursors: Array<any> = factories.filter(annoFactory => annoFactory.outputs[0].Product === input.Product)
             precursors.forEach(precursor => {
-                this.inputs.push(new Factory(precursor.guid))
+                this.inputs.push(new Factory(precursor.guid, false))
             });
         })
+
+        if (this.isRootFactory) {
+            this.minimumProductionTimeOfChain = this.calculateMinimumProductionTime()
+            this.calculateMinimumFactoryCount()
+            this.calculateChainBuildCost()
+            this.calculateChainMaintenance()
+         } 
+        
     }
+
+    /**
+     * Sums up the maintenance Objects of all Factories
+     *
+     * @return {Maintenance} A Maintenance Object containing all Maintenance workforce information
+     */
+    calculateChainMaintenance(): Maintenance {
+        if (!this.isRootFactory) throw Error("This is no root factory")
+        return new Maintenance()
+    }
+
+    _recursiveMaintenanceWalk(rootMaintenance: Maintenance)  {}
+
+    /**
+     * Sums up the maintenance Objects of all Factories
+     *
+     * @return {Maintenance} A Maintenance Object containing all Maintenance workforce information
+     */
+    calculateChainBuildCost(): BuildCost {
+        if (!this.isRootFactory) throw Error("This is no root factory")
+        return new BuildCost()
+    }
+
+    _recursiveBuildCostWalk(rootBuildCost: BuildCost)  {}
+
+    calculateMinimumProductionTime(): number {
+        if (!this.isRootFactory) throw Error("This is no root factory")
+        return 0
+    }
+
+    _recursiveProductionTimeWalk(curentMinimum: number): number  {
+        return 0
+    }
+
+    calculateMinimumFactoryCount(): void {
+        if (!this.isRootFactory) throw Error("This is not the Root Factory")
+        // how many factories are needed at least to build an efficient chain
+    }
+
+
+
 }
 
 class Maintenance{
@@ -64,6 +129,22 @@ class Maintenance{
         this.shepherds = 0
         this.elders = 0
         this.errors = 0
+    }
+
+    add(other: Maintenance): void {
+        this.gold += other.gold
+        this.farmers += other.farmers
+        this.workers += other.workers
+        this.artisans += other.artisans
+        this.engineers += other.engineers
+        this.scholars += other.scholars
+        this.jornaleros += other.jornaleros
+        this.obreros = +other.obreros
+        this.explorers += other.explorers
+        this.technicians += other.technicians
+        this.shepherds += other.shepherds
+        this.elders += other.elders
+        this.errors += other.errors
     }
 
     addByID(id: number, amount: number): boolean {
@@ -124,7 +205,7 @@ class Maintenance{
     }
 }
 
-class BuildMaterial{
+class BuildCost{
     wood: number
     bricks: number
     steel: number
@@ -144,6 +225,17 @@ class BuildMaterial{
         this.wanza = 0
         this.mudbricks = 0
         this.errors = 0
+    }
+
+    add(other: BuildCost): void {
+        this.wood += other.wood
+        this.bricks += other.bricks
+        this.steel += other.steel
+        this.windows += other.windows
+        this.concrete += other.concrete
+        this.wanza += other.wanza
+        this.mudbricks += other.mudbricks
+        this.errors += other.errors        
     }
 
     addByID(id: number, amount: number): boolean {
